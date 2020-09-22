202f959f92b59689c29cc5e0f92592674d28ee0f
Alex Waldron <awaldro1@stevens.edu>
2020-09-22 08:08:18 -0400

    Widget Construction

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State {
+var questIndex = 0;
+void answerQuestion() {
+  questIndex++;
+  print("Answer chosen");
+}
+
   @override
   Widget build(BuildContext context) {
+
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is my default text!"),
+        body: Column(children: [
+          Text(questions[questIndex]),
+          RaisedButton(
+            child: Text("Answer 1"),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text("Answer 2"),
+            onPressed: () => print("answer 2 chosen"),
+          ),
+          RaisedButton(
+            child: Text("Answer 3"),
+            onPressed: answerQuestion,
+          ),
+        ]),
       ),
     );
   }
