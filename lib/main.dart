bda7e00fc712fef672b8bad5e8b129401f4f010e
Alex Waldron <awaldro1@stevens.edu>
2020-09-22 20:11:31 -0400

    Began formatting and splitting widgets into seperate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyAppState extends State {
-var questIndex = 0;
-void answerQuestion() {
-  questIndex++;
-  print("Answer chosen");
-}
+class _MyAppState extends State<MyApp> {
+  var _questIndex = 0;
+  var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+      "What's your least favorite smell",
+    ];
+  void _answerQuestion() {
+    setState(() {
+      
+      if (_questIndex == questions.length-1) {
+        _questIndex = 0;
+      } else {
+        _questIndex++;
+      }
+    });
+    print("Answer chosen");
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    var questions = [
-      "What's your favorite color?",
-      "What's your favorite animal?",
-    ];
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
         body: Column(children: [
-          Text(questions[questIndex]),
+          Question(
+            questions[_questIndex],
+          ),
           RaisedButton(
             child: Text("Answer 1"),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text("Answer 2"),
-            onPressed: () => print("answer 2 chosen"),
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text("Answer 3"),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
         ]),
       ),
